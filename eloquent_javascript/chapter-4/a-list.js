25dbcc0d363cb55383b804a372f675b5534e923e
Aleksandre Clapin-Pepin <aclapinpepin@gmail.com>
2017-12-28 12:19:01 +0100

    Produce array from list

--- eloquent_javascript/chapter-4/a-list.js
+++ eloquent_javascript/chapter-4/a-list.js
@@ -6,4 +6,15 @@ function arrayToList(arr) {
   return list
 }
 
+function listToArray(list) {
+  arr = [];
+
+  for (var node = list; node; node = node.rest) {
+    arr.push(node.value);
+  }
+
+  return arr;
+}
+
 console.log(arrayToList([10, 20]));
+console.log(listToArray(arrayToList([10, 20, 30])));
